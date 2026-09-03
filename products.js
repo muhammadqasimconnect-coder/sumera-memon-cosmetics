d50ea68840c2a10eaa8cecc6a00a538e47fad42b
muhammadqasimconnect-coder <muhammadqasim.connect@gmail.com>
2026-09-03 23:42:00 +0500

    Add Lilybyred Korean lip tint product

--- products.js
+++ products.js
@@ -4,6 +4,7 @@ const PRODUCTS = [
     id: 1,
     name: "Morphe Highlighter Pencil",
     category: "makeup",
+    type: "Highlighter Pencil",
     price: 150,
     bundleQty: 2,
     bundlePrice: 250,
@@ -11,5 +12,18 @@ const PRODUCTS = [
     shades: ["Oh Brow Baby", "So Worth It"],
     description: "A handy highlighter pencil for adding definition and brightness to your makeup look. Its pencil format makes precise application easy. Available in two shades: Oh Brow Baby and So Worth It.",
     image: "assets/morphe-highlighter.jpg"
+  },
+  {
+    id: 2,
+    name: "Lilybyred Glassy Layer Fixing Tint",
+    category: "makeup",
+    type: "Korean Lip Tint",
+    price: 150,
+    bundleQty: 2,
+    bundlePrice: 250,
+    bundleGroup: "lilybyred-glassy-tint",
+    shades: ["One Colour"],
+    description: "A Korean lip tint with a smooth, lightweight feel and a glossy glass-like finish. An easy everyday pick for adding a polished pop of colour and shine to your lips. One colour available.",
+    image: "assets/lilybyred-glassy-tint.svg"
   }
 ];
